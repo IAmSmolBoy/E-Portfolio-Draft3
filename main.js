1b2c10cc087876fc711ff56567cbd978ed29e251
IAmSmolBoy <65689332+IAmSmolBoy@users.noreply.github.com>
2020-06-26 21:30:17 +0800

    -

--- main.js
+++ main.js
@@ -5,22 +5,36 @@ const ThreeLines=document.getElementById("ThreeLines")
 const DropDownMenu=document.getElementById("DropDownMenu")
 const OtherDropDown=document.getElementsByClassName("OtherDropDown")
 const OtherDropDownMenu=document.getElementById("OtherDropDownMenu")
+var Dropped=false
 function Dropdown(){
-    for(i=1;i<=DropDown.length;i++){
-        if(DropDown[i-1].style.display!="block"){
-            DropDown[i-1].style.display="block"
-            DropDownMenu.style.animation="DropDown 0.25s linear"
-            OtherDropDown[i-1].style.display="block"
-            OtherDropDownMenu.style.animation="MoveLeft 0.25s linear"
-            DropDownMenu.style.top=`${parseInt(navbar.clientHeight)}px`
+    if(Dropped===false){
+        console.log("Hello")
+        DropDownMenu.style.animation="DropDown 0.25s linear"
+        DropDownMenu.style.top=`${parseInt(navbar.clientHeight)}px`
+        OtherDropDownMenu.style.animation="MoveLeft 0.25s linear"
+        Dropped=true
+    }
+    else{
+        console.log("There")
+        DropDownMenu.style.animation=""
+        OtherDropDownMenu.style.animation=""
+        Dropped=false
+    }
+    
+    for(i=0;i<DropDown.length;i++){
+        if(DropDown[i].style.display!="block"){
+            DropDown[i].style.display="block"
         }
         else{
-            for(i=1;i<=DropDown.length;i++){
-                DropDown[i-1].style.display="none"
-                DropDownMenu.style.animation=""
-                OtherDropDown[i-1].style.display="none"
-                OtherDropDownMenu.style.animation=""
-            }
+            DropDown[i].style.display="none"
+        }
+    }
+    for(j=0;j<OtherDropDown.length;j++){
+        if(OtherDropDown[j].style.display!="block"){
+            OtherDropDown[j].style.display="block"
+        }
+        else{
+            OtherDropDown[j].style.display="none"
         }
     }
 }
@@ -34,10 +48,5 @@ const GithubLogo = document.getElementById("Githublogo")
 
 // Copy text code
 function CopyText(){
-    const el = document.createElement('textarea');
-    el.value = "gohhongrui@gmail.com";
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
+    navigator.clipboard.writeText("gohhongrui@gmail.com")
 }
