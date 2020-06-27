56364cbb10f0f6bb9447a56371bfaa5fdcbfbf97
IAmSmolBoy <65689332+IAmSmolBoy@users.noreply.github.com>
2020-06-27 20:06:45 +0800

    -

--- navbar.js
+++ navbar.js
@@ -1,4 +1,3 @@
-// Three lines button onclick opens up navbar and dropdown menu
 const Navbar=document.getElementById("navbar")
 const DropDown=document.getElementsByClassName("DropDown")
 const ThreeLines=document.getElementById("ThreeLines")
@@ -7,20 +6,6 @@ const OtherDropDown=document.getElementsByClassName("OtherDropDown")
 const OtherDropDownMenu=document.getElementById("OtherDropDownMenu")
 var Dropped=false
 function Dropdown(){
-    if(Dropped===false){
-        console.log("Hello")
-        DropDownMenu.style.animation="DropDown 0.25s linear"
-        DropDownMenu.style.top=`${parseInt(navbar.clientHeight)}px`
-        OtherDropDownMenu.style.animation="MoveLeft 0.25s linear"
-        Dropped=true
-    }
-    else{
-        console.log("There")
-        DropDownMenu.style.animation=""
-        OtherDropDownMenu.style.animation=""
-        Dropped=false
-    }
-    
     for(i=0;i<DropDown.length;i++){
         if(DropDown[i].style.display!="block"){
             DropDown[i].style.display="block"
@@ -37,16 +22,15 @@ function Dropdown(){
             OtherDropDown[j].style.display="none"
         }
     }
-}
-
-
-// Socials logo onlick, socials info pop up
-const DiscordLogo = document.getElementById("discordlogo")
-const GithubLogo = document.getElementById("Githublogo")
-
-
-
-// Copy text code
-function CopyText(){
-    navigator.clipboard.writeText("gohhongrui@gmail.com")
+    if(Dropped===false){
+        DropDownMenu.style.animation="DropDown 0.25s linear"
+        DropDownMenu.style.top=`${parseInt(navbar.clientHeight)}px`
+        OtherDropDownMenu.style.animation="MoveLeft 0.25s linear"
+        Dropped=true
+    }
+    else{
+        DropDownMenu.style.animation=""
+        OtherDropDownMenu.style.animation=""
+        Dropped=false
+    }
 }
